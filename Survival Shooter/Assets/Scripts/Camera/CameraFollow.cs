23bc91a15a54cfa5b3fe7419a7dd566eaa1e822c
Sooham Rafiz <rafizsooham@gmail.com>
2015-06-15 23:40:04 -0400

    added finished build

---
+++ Survival Shooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		// Calculate the initial offset
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Create a position the camera is aiming for based on the offset of from the target
+		Vector3 targetCamPos = target.position + offset;
+
+		// Smoothly move
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
